0988fdbc1e72db06c22df5ef0f6b1c43fe411ef2
wdtwo <wdtwo.com@gmail.com>
2024-12-16 23:16:43 +0800

    1

--- nuxtProject/nuxt.config.ts
+++ nuxtProject/nuxt.config.ts
@@ -1,25 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr:false,
+
   css:[
-    'public/basic.css'
+    'public/basic.css',
+    'public/markdown.css'
   ],
+
   plugins: [
     '~/plugins/element.ts',
-    "@/plugins/hightlight"
+    // "@/plugins/hightlight"
   ],
+
   devtools: { enabled: true },
   modules: ['@nuxt/content'],
+
   routeRules: {
     '/': { prerender: true }
   },
+
   build: {
     transpile: (process.env.NODE_ENV === 'development' ? [] : ['element-plus'])
   },
+
   app: {
     pageTransition: { name: 'page', mode: 'out-in' }
   },
+
   content: {
     documentDriven: true
-  }
+  },
+
+  compatibilityDate: '2024-12-14'
 })
--- nuxtProject/pages/posts/[...slug].vue
+++ nuxtProject/pages/posts/[...slug].vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
-</script>
 
+
+</script>
 
 <style scoped>
   .content {
     background-color: white;
-    padding:25px;
+    padding:25px 5px 25px 25px;
   }
 </style>
 <template>
   <main class="content">
-    <ContentDoc />
+    <ContentDoc class="markdown-content"/>
   </main>
 </template>
