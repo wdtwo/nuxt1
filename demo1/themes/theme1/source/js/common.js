b51ca5295ced3b7167a40279da8016df434a5c6c
wangjun <wangjun@ethank.com>
2024-12-24 18:04:35 +0800

    123

---
+++ demo1/themes/theme1/source/js/common.js
@@ -0,0 +1,47 @@
+window.onload = function() {
+    // 获取当前主题
+    const theme = sessionStorage.getItem('theme');
+    if(theme){
+        document.body.className = theme;
+    }else{
+        // 当前时间
+        const now = new Date();
+        const hours = now.getHours();
+        document.body.className = (hours > 18 || hours < 6) ? 'night' : 'light';
+        sessionStorage.setItem('theme', document.body.className);
+    }
+    // 监听点击事件
+    document.querySelector('.fa-sun').addEventListener('click', function() {
+        document.body.className = 'night';
+        sessionStorage.setItem('theme', 'night');
+    });
+    document.querySelector('.fa-moon').addEventListener('click', function() {
+        document.body.className = 'light';
+        sessionStorage.setItem('theme', 'light');
+    });
+
+    // 导航栏点击事件
+    // 搜索按钮点击事件
+    document.querySelector('.fa-search').addEventListener('click',function(){
+        // 重置表单
+        document.querySelector('.search-page-form').reset();
+        
+        const searchPage = document.querySelector('.search-page');
+        searchPage.style.display = 'block';
+        searchPage.classList.add('fadeIn');
+        searchPage.addEventListener('animationend', () => {
+            searchPage.style.display = 'block';
+            searchPage.classList.remove('fadeIn');
+            // 聚焦输入框
+            document.querySelector('[name="keyword"]').focus();
+        });
+    })
+    document.querySelector('.search-page-bg').addEventListener('click',function(){
+        const searchPage = document.querySelector('.search-page');
+        searchPage.classList.add('fadeOut');
+        searchPage.addEventListener('animationend', () => {
+            searchPage.style.display = 'none';
+            searchPage.classList.remove('fadeOut');
+        });
+    })
+}
